2a8ffe49f91a5bbc7dd07f39564897cf9e4564ad
Daniel Hakimi-Nayeri <Daniel@Daniels-MacBook-Pro.local>
2012-08-09 17:52:55 +0100

    Initial Commit

---
+++ EuroTalkApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EuroTalkApp
+//
+//  Created by Daniel Hakimi-Nayeri on 09/08/2012.
+//  Copyright (c) 2012 Daniel Hakimi-Nayeri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EuroTalkApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EuroTalkApp
+//
+//  Created by Daniel Hakimi-Nayeri on 09/08/2012.
+//  Copyright (c) 2012 Daniel Hakimi-Nayeri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
